b6b1852113692773a174318a404cfb4af16bf4d8
Ashkan Tariverdi <ashkantariverdi82@gmail.com>
2023-01-27 23:18:05 +0330

    cat & create added without quotation

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <direct.h>
 #include <dirent.h> 
 #include <sys/types.h>
+
 
 #define MAX 10000
 
@@ -20,18 +22,30 @@ void FindCommand();
 
 int main()
 {
-    scanf("%[^ ]s", command);
+    while(1)
+    {
+    chdir("root");
+    scanf("%s", command);
     FindCommand();
+    }
 
     return 0;    
 }
 
 void FindCommand()
 {
-    if(strcmp(command, "cat") == 0) 
+    if(!strcmp(command, "cat")) {
         cat();
-    else 
-        printf("Invalid Command!\nSimply type <--help> for more information.");
+        return;
+    } 
+    else if(!strcmp(command, "createfile")) {
+        createfile();
+        return;
+    }
+    else {
+        printf("Invalid Command!\nSimply type <--help> for more information.\n");
+        return;
+    }
 
 }
 
@@ -41,24 +55,78 @@ void cat()
     scanf(" /root/%[^\n]s", address);
 
     if(strcmp(command_extension, "--file") != 0) {
-        printf("Invalid Command!\nSimply type <--help> for more information.");
+        printf("Invalid Command!\nSimply type <--help> for more information.\n");
         return;
     }
 
-    int str[MAX];
+    // int str[MAX];
+    char str;
     filepointer = fopen(address , "r");
     if(filepointer != NULL) {
-        while(fgets(str, MAX, filepointer)) {
-        printf("%s", str);
+        str = fgetc(filepointer);
+        while(str != EOF) {
+            printf("%c", str);
+            str = getc(filepointer);
+            // printf("Fuck");
         }
-    } else {
-        printf("No Such A File!");
+        printf("\n");
+    }else {
+        printf("No Such A File!\n");
+        return;
     }
     fclose(filepointer);
 }
 
 void createfile()
 {
+    char *directory;
+    char *next_directory;
+    char *new_address;
+    int counter = 0;
+    int result;
+    
+
     scanf(" %[^ ]s", command_extension);
-    scanf(" /root/%[^\n]s", address);    
+    if(strcmp(command_extension, "--file") != 0) {
+        printf("Invalid Command!\nSimply type <--help> for more information.");
+        return;
+    }    
+
+    scanf("%[^\n]s", address);    
+    // strcpy(cpy_address, address);
+
+    directory = strtok(address, "/");
+    directory = strtok(NULL, "/");
+    directory = strtok(NULL, "/");
+
+    // printf("%s\n", directory);
+    while(directory != NULL)
+    {
+        counter++;
+        next_directory = strtok(NULL, "/");
+        if(next_directory == NULL) {
+            if(!access(directory, F_OK)) {
+                printf("File already exist!\n");
+                return;
+            }
+            filepointer = fopen(directory, "a");
+            fclose(directory);
+            break;
+        }
+        result = mkdir(directory); 
+        chdir(directory);
+
+        // mkdir(directory);
+        strcpy(directory, next_directory);
+    }
+    if(!result)
+    printf("File created successfully!\n");
+    // printf("count = %d\n", counter);
+    
+    while(counter != 1)
+    {
+        chdir("..");
+        counter--;
+    }
+    return;
 }
